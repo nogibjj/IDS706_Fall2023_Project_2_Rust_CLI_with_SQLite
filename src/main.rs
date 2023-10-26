04e2176c779de6c052ba3cd7d451018402caf7de
yz674 <yz674@duke.edu>
2023-10-26 11:29:45 -0400

    Implement CRUD

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use rusqlite::{params, Connection, Result};
+
+fn main() -> Result<()> {
+    // Connect to the database
+    let conn = Connection::open("my_database.db")?;
+
+    // Create a new table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS people (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            age INTEGER
+         )",
+        [],
+    )?;
+
+    // Insert two records
+    conn.execute(
+        "INSERT INTO people (name, age) VALUES (?, ?)",
+        params!["Alice", 30],
+    )?;
+    conn.execute(
+        "INSERT INTO people (name, age) VALUES (?, ?)",
+        params!["Bob", 25],
+    )?;
+
+    // Update Alice's age
+    conn.execute(
+        "UPDATE people SET age = ? WHERE name = ?",
+        params![31, "Alice"],
+    )?;
+
+    // Print all records
+    let mut stmt = conn.prepare("SELECT id, name, age FROM people")?;
+    let rows = stmt.query_map([], |row| {
+        Ok((row.get(0)?, row.get(1)?, row.get(2)?))
+    })?;
+
+    for row in rows {
+        let (id, name, age): (i32, String, i32) = row?;
+        println!("id = {}, name = {}, age = {}", id, name, age);
+    }
+
+    // Delete Bob's record
+    conn.execute("DELETE FROM people WHERE name = ?", params!["Bob"])?;
+
+    Ok(())
+}
